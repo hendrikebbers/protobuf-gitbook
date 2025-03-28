666eb940557e53bc778a7e9c2a024f99e1d3b281
Hendrik Ebbers <hendrik.ebbers@web.de>
2025-03-28 19:27:46 +0100

    PERFECT WORK!

--- create-proto-doc.sh
+++ create-proto-doc.sh
@@ -4,7 +4,6 @@ hapi_version="v0.61.0"
 build_dir="build"
 consensus_node_dir="$build_dir/hiero-consensus-node"
 protos_dir="$build_dir/protos"
-merged_protos_dir="$build_dir/merged-protos"
 docs_dir="$build_dir/docs"
 
 echo "Step 1: Prepare directories"
@@ -12,8 +11,6 @@ mkdir -p $consensus_node_dir
 rm -rf $consensus_node_dir/*
 mkdir -p $protos_dir
 rm -rf $protos_dir/*
-mkdir -p $merged_protos_dir
-rm -rf $merged_protos_dir/*
 mkdir -p $docs_dir
 rm -rf $docs_dir/*
 
@@ -23,17 +20,25 @@ curl -sL "https://github.com/hiero-ledger/hiero-consensus-node/archive/refs/tags
 cp -a $consensus_node_dir/hapi/hedera-protobuf-java-api/src/main/proto/. $protos_dir
 
 
-echo "Step 3: Merge all protobuf files in 1 folder"
-cp -a $protos_dir/block/. $merged_protos_dir
-cp -a $protos_dir/mirror/. $merged_protos_dir
-cp -a $protos_dir/platform/. $merged_protos_dir
-cp -a $protos_dir/sdk/. $merged_protos_dir
-cp -a $protos_dir/services/. $merged_protos_dir
-#cp -a $protos_dir/streams/. $merged_protos_dir
+echo "Step 3: Create Doc Files"
+# Loop through each directory
+for DIR in "${protos_dir[@]}"; do
+  echo "Searching in $DIR..."
+  
+  # Find all .proto files in the directory
+  find "$DIR" -name "*.proto" | while read -r PROTO_FILE; do
+    echo "Processing $PROTO_FILE"
+    
+    # Create a safe file name for the doc output
+    DOC_NAME=$(basename "$PROTO_FILE" .proto).md
 
-echo "Step 4: Create Doc Files"
-docker run --rm \
+    # Generate markdown docs using protoc-gen-doc
+    echo "START for ${PROTO_FILE#build/}"
+    docker run --rm \
   -v $(pwd)/$docs_dir:/out \
-  -v $(pwd)/$merged_protos_dir:/protos \
+  -v $(pwd)/$protos_dir:/protos \
    -v $(pwd)/template:/template \
-  pseudomuto/protoc-gen-doc --doc_opt=/template/template.mustache,doc.txt
+  pseudomuto/protoc-gen-doc --doc_opt=markdown,"$DOC_NAME" "${PROTO_FILE#build/}"
+  echo "END for ${PROTO_FILE#build/}"
+  done
+done
